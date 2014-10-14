6f793859a2a420626e79fed8d1af051ac090c2eb
Stephen Young <StephenJayYoung@gmail.com>
2014-10-14 14:12:14 -0700

    we have a passing test

--- test/app/test.js
+++ test/app/test.js
@@ -1,8 +1,15 @@
 'use strict';
 
 describe('app', function() {
-  it('has index stuff', function() {
+  it.skip('has index stuff', function() {
     visit('/');
     expect(find('.index-stuff').length).to.eql(1);
+  });
+
+  it('routes to an insult', function() {
+  	// find() is like $()
+  	// the argument to find is a CSS selector
+    visit('/insult');
+  	expect(find('span.insult').length).to.eql(1);
   });
 });
