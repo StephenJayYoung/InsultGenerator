5707eacedb6f63a9fdedcd8d6df3d4689b87e926
Stephen Young <StephenJayYoung@gmail.com>
2014-10-15 14:55:39 -0700

    added compliment.js

---
+++ app/scripts/compliment.js
@@ -0,0 +1,7 @@
+module.exports = [
+  'You are supah dupah', 'You are beautiful'
+];
+
+
+//write a function that iterates through an array 
+//and randomly selects an item in the array. use math.ranom()?
